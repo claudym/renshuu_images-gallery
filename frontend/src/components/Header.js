ab788e2ca78ae1e119e402efa591900843a1d5f9
claudym <claudy.m.mejia@gmail.com>
2022-01-31 16:38:57 -0400

    controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,11 +7,6 @@ const Header = ({ title }) => {
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/">{title}</Navbar.Brand>
-        {/* <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav> */}
       </Container>
     </Navbar>
   );
